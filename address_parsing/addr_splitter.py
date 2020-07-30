e4fbd0bab153b087f144e47b9c96fac5a933dad0
fishinvest <fishinvest@gmail.com>
2020-07-30 18:05:33 -0400

    Several smaller coding changes, adding more docstrings

--- address_parsing/addr_splitter.py
+++ address_parsing/addr_splitter.py
@@ -8,27 +8,49 @@ Created on Fri Jul 17 05:03:14 2020
 import re
 import pandas as pd
 
-def split_df_addresses(addr_df):
-    TempIDZ = 0
+def split_df_addresses(addr_df: pd.DataFrame):
+    '''
+
+
+    Parameters
+    ----------
+    addr_df : pd.DataFrame
+        This is a DataFrame containing your list of addresses. Specifically, this
+        needs to have two columns, 'TempID' and 'parsed_Addr'. TempID contains the
+        unique ID for the address entry, and parsed_Addr contains the parsed output
+        from the address parser
+
+    Returns
+    -------
+    return_df : pd.DataFrame
+        This returns all of the addresses provided as input, but multiple addresses
+        and ranges have been separated into different lines. For instance, address
+        "1-9 west street" will be converted into 5 lines, "1 west street", "3 west
+        street", ..., "9 west street". Each of these will have a column with their
+        original TempID, but also now have a TempIDZ which is unique to each line.
+
+    '''
+    temp_idz = 0
     return_list = []
-    
-    for idx, row in addr_df.iterrows():
+
+    for row in addr_df.iterrows():
         addresses = row.parsed_Addr
         for address in addresses:
-            if re.match('\d+-\d+.+', address) is not None:
+            if re.match('\\d+-\\d+.+', address) is not None:
                 print(address)
-                start = re.search('(\d+)-(\d+)', address).group(1)
-                end = re.search('(\d+)-(\d+)', address).group(2)
-                partial_address = re.search('(\d+)-(\d+) (.+)', address).group(3)
-                for x in range(int(start), int(end), 2):
-                    TempIDZ = TempIDZ + 1
-                    single_address =f'{x} {partial_address}'
-                    return_list.append({'TempID':row.TempID,'TempIDZ':TempIDZ,'address':single_address})
+                start = re.search('(\\d+)-(\\d+)', address).group(1)
+                end = re.search('(\\d+)-(\\d+)', address).group(2)
+                partial_address = re.search('(\\d+)-(\\d+) (.+)', address).group(3)
+                for r_del in range(int(start), int(end), 2):
+                    temp_idz = temp_idz + 1
+                    single_address = f'{r_del} {partial_address}'
+                    return_list.append({'TempID':row.TempID, 'TempIDZ':temp_idz,
+                                        'address':single_address})
             else:
-                TempIDZ = TempIDZ + 1
-                return_list.append({'TempID':row.TempID,'TempIDZ':TempIDZ,'address':address})
-        
-    
+                temp_idz = temp_idz + 1
+                return_list.append({'TempID':row.TempID, 'TempIDZ':temp_idz,
+                                    'address':address})
+
+
     return_df = pd.DataFrame(return_list)
     return return_df
-
--- address_parsing/algo_grammar.py
+++ address_parsing/algo_grammar.py
@@ -7,7 +7,8 @@ Created on Thu Jul  2 22:26:36 2020
 
 import re
 
-class Addr_Parser:
+class AddrParser:
+
     def __init__(self, address, testing=False):
         self.addresses = []
         self.flags = []
@@ -15,13 +16,13 @@ class Addr_Parser:
         address = re.sub('\\.', '', address)
         if '(' in address:
             address = re.sub('\\(.+\\)', '', address)
-            self.flags.append(Addr_Parser.Token('Err_02', typ='flag'))
+            self.flags.append(AddrParser.Token('Err_02', typ='flag'))
         address = address.lower()
         pieces = re.split('( and |&|#|,|;|//|/| |-)', address)
         pieces = [i.strip() for i in pieces if i]
-        pieces = [Addr_Parser.Token(i) for i in pieces if i]
+        pieces = [AddrParser.Token(i) for i in pieces if i]
         pieces = [i for i in pieces if i.strng != " "]
-        
+
         if testing:
             print("Before:", end="")
             print(pieces, end="\n\n")
@@ -80,7 +81,7 @@ class Addr_Parser:
         if testing:
             print("After remove_addrs      : ", end="")
             print(pieces, end="\n\n")
-            
+
         pieces = self.remove_flags(pieces)
         if testing:
             print("After remove_flags      : ", end="")
@@ -88,7 +89,7 @@ class Addr_Parser:
 
         for piece in pieces:
             if piece.typ != 'del':
-                self.flags.append(Addr_Parser.Token('Err_06', typ='flag'))
+                self.flags.append(AddrParser.Token('Err_06', typ='flag'))
 
     class Token:
         def __init__(self, strng, typ='other'):
@@ -132,37 +133,37 @@ class Addr_Parser:
             return f"'{self.strng}':'{self.typ}'"
 
     Error_Codes = {'Err_01':'No Numeric Found',
-    'Err_02':'Parenthesis in address',
-    'Err_03':'Apartment token without valid token following',
-    'Err_04':'Possible bad range order',
-    'Err_05':'Badly formed address',
-    'Err_06':'Extra token after parsing',
-    'Inf_01':'Contains Range Delimiter',
-    'Inf_02':'Long Number',
-    'Inf_03':'Ambiguous Direction, Unit',
-    'Inf_04':'Has numbers divided by slash'
-    }
+                   'Err_02':'Parenthesis in address',
+                   'Err_03':'Apartment token without valid token following',
+                   'Err_04':'Possible bad range order',
+                   'Err_05':'Badly formed address',
+                   'Err_06':'Extra token after parsing',
+                   'Inf_01':'Contains Range Delimiter',
+                   'Inf_02':'Long Number',
+                   'Inf_03':'Ambiguous Direction, Unit',
+                   'Inf_04':'Has numbers divided by slash'
+                   }
 
     @staticmethod
     def initial_checks(pieces):
         has_num = False
         possible_range = False
-        hasNumSlash=False
+        has_num_slash = False
         for idx, piece in enumerate(pieces):
             if piece.typ == 'num':
                 has_num = True
             if piece.typ == 'rdel':
                 possible_range = 'True'
             if piece.typ == 'slash':
-                if (piece[idx-1].typ=='num') and (piece[idx-1].typ=='num'):
-                    hasNumSlash=True
+                if (piece[idx-1].typ == 'num') and (piece[idx-1].typ == 'num'):
+                    has_num_slash = True
 
         if not has_num:
-            pieces.append(Addr_Parser.Token('Err_01', typ='flag'))
+            pieces.append(AddrParser.Token('Err_01', typ='flag'))
         if possible_range:
-            pieces.append(Addr_Parser.Token('Inf_01', typ='flag'))
-        if hasNumSlash:
-            pieces.append(Addr_Parser.Token('Inf_04', typ='flag'))
+            pieces.append(AddrParser.Token('Inf_01', typ='flag'))
+        if has_num_slash:
+            pieces.append(AddrParser.Token('Inf_04', typ='flag'))
 
         return pieces
 
@@ -194,7 +195,7 @@ class Addr_Parser:
         for piece in pieces:
             if piece.typ == 'num':
                 if len(piece.strng) > 5:
-                    pieces.append(Addr_Parser.Token("Inf_02", typ='flag'))
+                    pieces.append(AddrParser.Token("Inf_02", typ='flag'))
         return pieces
 
     @staticmethod
@@ -207,14 +208,14 @@ class Addr_Parser:
             if piece.typ == 'alnum':
                 # If it is a combined address and direction
                 if re.match("^\\d+(?:n|e|s|w)$", piece.strng):
-                    temp_piece = Addr_Parser.Token(piece.strng[-1:], typ="char")
+                    temp_piece = AddrParser.Token(piece.strng[-1:], typ="char")
                     piece.strng = piece.strng[:-1]
                     piece.typ = 'num'
                     pieces.insert(i, temp_piece)
-                    temp_pieces.append(Addr_Parser.Token("Inf_03", typ='flag'))
+                    temp_pieces.append(AddrParser.Token("Inf_03", typ='flag'))
                 # If it is a combined address and direction
                 elif re.match("^\\d+(?:nw|ne|sw|se)$", piece.strng):
-                    temp_piece = Addr_Parser.Token(piece.strng[-2:], typ="char")
+                    temp_piece = AddrParser.Token(piece.strng[-2:], typ="char")
                     piece.strng = piece.strng[:-2]
                     piece.typ = 'num'
                     pieces.insert(i, temp_piece)
@@ -235,15 +236,15 @@ class Addr_Parser:
         while len(pieces) > 1:
             if pieces[0].typ == 'pound':
                 if (pieces[1].typ in ['char', 'num', 'multnum', 'range', 'alnum', 'pound']):
-                    space = " " if len(pieces[0].strng)>1 else ""
-                    pieces[0] = Addr_Parser.Token(f'{pieces[0].strng}{space}{pieces[1].strng}', typ='pchar')
+                    space = " " if len(pieces[0].strng) > 1 else ""
+                    pieces[0] = AddrParser.Token(f'{pieces[0].strng}{space}{pieces[1].strng}', typ='pchar')
                     del pieces[1]
                 else:
-                    error_pieces.append(Addr_Parser.Token('Err_03', typ='flag'))
+                    error_pieces.append(AddrParser.Token('Err_03', typ='flag'))
                     del pieces[0]
             elif pieces[0].typ == 'pchar':
                 if (pieces[1].typ in ['char', 'num', 'multnum', 'range', 'alnum']):
-                    pieces[0] = Addr_Parser.Token(f'{pieces[0].strng} {pieces[1].strng}', typ='pchar')
+                    pieces[0] = AddrParser.Token(f'{pieces[0].strng} {pieces[1].strng}', typ='pchar')
                     del pieces[1]
                 else:
                     temp_pieces.append(pieces[0])
@@ -262,11 +263,11 @@ class Addr_Parser:
         temp_pieces = []
         while len(pieces) > 2:
             if pieces[0].typ == 'char':
-                if pieces[1].typ in ['char','num', 'alnum']:
-                    pieces[0] = Addr_Parser.Token(f'{pieces[0].strng} {pieces[1].strng}', typ='char')
+                if pieces[1].typ in ['char', 'num', 'alnum']:
+                    pieces[0] = AddrParser.Token(f'{pieces[0].strng} {pieces[1].strng}', typ='char')
                     del pieces[1]
                 elif(pieces[1].strng in ['&', 'and']) and (pieces[2].typ == 'char'):
-                    pieces[0] = Addr_Parser.Token(f'{pieces[0].strng} {pieces[1].strng} {pieces[2].strng}', typ='char')
+                    pieces[0] = AddrParser.Token(f'{pieces[0].strng} {pieces[1].strng} {pieces[2].strng}', typ='char')
                     del pieces[2]
                     del pieces[1]
                 else:
@@ -277,8 +278,8 @@ class Addr_Parser:
                 del pieces[0]
         while len(pieces) > 1:
             if pieces[0].typ == 'char':
-                if pieces[1].typ in ['char','num', 'alnum']:
-                    pieces[0] = Addr_Parser.Token(f'{pieces[0].strng} {pieces[1].strng}', typ='char')
+                if pieces[1].typ in ['char', 'num', 'alnum']:
+                    pieces[0] = AddrParser.Token(f'{pieces[0].strng} {pieces[1].strng}', typ='char')
                     del pieces[1]
                 else:
                     temp_pieces.append(pieces[0])
@@ -296,7 +297,7 @@ class Addr_Parser:
         while len(pieces) > 1:
             if pieces[0].typ in ['num', 'multnum', 'range']:
                 if pieces[1].typ in ['num', 'range']:
-                    pieces[0] = Addr_Parser.Token(f'{pieces[0].strng} {pieces[1].strng}', typ='multnum')
+                    pieces[0] = AddrParser.Token(f'{pieces[0].strng} {pieces[1].strng}', typ='multnum')
                     del pieces[1]
                 else:
                     temp_pieces.append(pieces[0])
@@ -312,7 +313,7 @@ class Addr_Parser:
         while len(pieces) > 2:
             if pieces[0].typ in ['num', 'multnum', 'range']:
                 if (pieces[1].typ == 'del') and (pieces[2].typ in ['num', 'range']):
-                    pieces[0] = Addr_Parser.Token(f'{pieces[0].strng} {pieces[2].strng}', typ='multnum')
+                    pieces[0] = AddrParser.Token(f'{pieces[0].strng} {pieces[2].strng}', typ='multnum')
                     del pieces[2]
                     del pieces[1]
                 else:
@@ -331,8 +332,8 @@ class Addr_Parser:
         temp_pieces = []
         while len(pieces) > 2:
             if pieces[1].typ == 'rdel':
-                if (pieces[0].typ in ['char','num','alnum']) and (pieces[2].typ in ['char','num','alnum']):
-                    pieces[1] = Addr_Parser.Token(f'{pieces[0].strng}-{pieces[2].strng}', typ='range')
+                if (pieces[0].typ in ['char', 'num', 'alnum']) and (pieces[2].typ in ['char', 'num', 'alnum']):
+                    pieces[1] = AddrParser.Token(f'{pieces[0].strng}-{pieces[2].strng}', typ='range')
                     del pieces[2]
                     del pieces[0]
                 else:
@@ -353,7 +354,7 @@ class Addr_Parser:
                     piece2 = piece1[:len(piece1)-len(piece2)] + piece2
                     piece.strng = piece1 + "-" + piece2
                 if piece2 < piece1:
-                    temp_pieces.append(Addr_Parser.Token('Err_04', typ='flag'))
+                    temp_pieces.append(AddrParser.Token('Err_04', typ='flag'))
 
         return temp_pieces
 
@@ -366,9 +367,9 @@ class Addr_Parser:
                 if pieces[1].typ == 'char':
                     if pieces[0].typ == 'multnum':
                         for num in pieces[0].strng.split():
-                            temp_pieces.append(Addr_Parser.Token(f'{num} {pieces[1].strng}', typ='address'))
+                            temp_pieces.append(AddrParser.Token(f'{num} {pieces[1].strng}', typ='address'))
                     else:
-                        temp_pieces.append(Addr_Parser.Token(f'{pieces[0].strng} {pieces[1].strng}', typ='address'))
+                        temp_pieces.append(AddrParser.Token(f'{pieces[0].strng} {pieces[1].strng}', typ='address'))
                     del pieces[1]
                     del pieces[0]
                 else:
@@ -380,7 +381,7 @@ class Addr_Parser:
         for piece in pieces:
             temp_pieces.append(piece)
         if error:
-            temp_pieces.append(Addr_Parser.Token('Err_05', typ='flag'))
+            temp_pieces.append(AddrParser.Token('Err_05', typ='flag'))
         return temp_pieces
 
     @staticmethod
@@ -389,11 +390,11 @@ class Addr_Parser:
         while len(pieces) > 2:
             if pieces[0].typ == 'address':
                 if (pieces[1].typ == 'del') and (pieces[2].typ == 'pchar'):
-                    pieces[0] = Addr_Parser.Token(f'{pieces[0].strng} {pieces[2].strng}', typ='address')
+                    pieces[0] = AddrParser.Token(f'{pieces[0].strng} {pieces[2].strng}', typ='address')
                     del pieces[2]
                     del pieces[1]
-                elif (pieces[1].typ == 'pchar'):
-                    pieces[0] = Addr_Parser.Token(f'{pieces[0].strng} {pieces[1].strng}', typ='address')
+                elif pieces[1].typ == 'pchar':
+                    pieces[0] = AddrParser.Token(f'{pieces[0].strng} {pieces[1].strng}', typ='address')
                     del pieces[1]
                 else:
                     temp_pieces.append(pieces[0])
@@ -403,8 +404,8 @@ class Addr_Parser:
                 del pieces[0]
         while len(pieces) > 1:
             if pieces[0].typ == 'address':
-                if (pieces[1].typ == 'pchar'):
-                    pieces[0] = Addr_Parser.Token(f'{pieces[0].strng} {pieces[1].strng}', typ='address')
+                if pieces[1].typ == 'pchar':
+                    pieces[0] = AddrParser.Token(f'{pieces[0].strng} {pieces[1].strng}', typ='address')
                     del pieces[1]
                 else:
                     temp_pieces.append(pieces[0])
@@ -424,7 +425,7 @@ class Addr_Parser:
             else:
                 temp_pieces.append(piece)
         return temp_pieces
-    
+
     def remove_flags(self, pieces):
         temp_pieces = []
         for piece in pieces:
