0932121c4866a5f6104d19d28e168eafb1aede5f
fishinvest <fishinvest@gmail.com>
2020-09-03 21:27:19 -0400

    Added first tests in pytest framework

---
+++ tests/address_parser_test.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 31 07:09:51 2020
+
+@author: fisherd
+"""
+
+import os
+
+os.chdir("C:\\Users\\fisherd\\Desktop\\projects\\CREDA\\")
+
+import pytest
+
+from CREDA_tools.address_parsing import algo_grammar
+
+def test_canAssertTrue():
+    assert True
+    
+def test_canCreateObject():
+    temp = algo_grammar.AddrParser('8 Queensland Ct')
+    assert type(temp) is algo_grammar.AddrParser
+
+def test_basicParse():
+    temp = algo_grammar.AddrParser('8 Queensland Ct')
+    assert temp.get_addrs() == ['8 queensland ct']
+    
+def test_parens():
+    temp = algo_grammar.AddrParser('100 jerusalem ave (hampshire house apts)')
+    assert temp.get_addrs() == ['100 jerusalem ave']
+    
+def test_commaDelimiter():
+    temp = algo_grammar.AddrParser('2221, 2223 & 2227 washington st')
+    assert temp.get_addrs() == ['2221 washington st', '2223 washington st', '2227 washington st']
+
+def test_andDelimiter():
+    temp = algo_grammar.AddrParser('825 and 831 k st')
+    assert temp.get_addrs() == ['825 k st', '831 k st']
+    
+def test_andDelimiter_2():
+    temp = algo_grammar.AddrParser('1200 electric avenue and 499 santa clara ave')
+    assert temp.get_addrs() == ['1200 electric avenue', '499 santa clara ave']
+
+def test_andDelimiter_3():
+    temp = algo_grammar.AddrParser('2810 and 2820 w charleston blvd')
+    assert temp.get_addrs() == ['2810 w charleston blvd', '2820 w charleston blvd']
+    
+def test_andInWord():
+    temp = algo_grammar.AddrParser('111 and 223 w anderson ln')
+    assert temp.get_addrs() == ['111 w anderson ln', '223 w anderson ln']
+
+def test_andInWord_2():
+    temp = algo_grammar.AddrParser('430 buckland hills drive and 110 slater st')
+    assert temp.get_addrs() == ['430 buckland hills drive', '110 slater st']
+
+'''    
+def test_hwy():
+    temp = algo_grammar.AddrParser('3000 hwy 5 and 7090 arbor pkwy')
+    assert temp.get_addrs() == ['3000 hwy 5', '7090 arbor pkwy']
+'''
+
+def test_semicolonDelimiter():
+    temp = algo_grammar.AddrParser('1337 n fiddlesticks place; 2421 cornerstone place')
+    assert temp.get_addrs() == ['1337 n fiddlesticks place', '2421 cornerstone place']
+    
+def test_spaceDelimiter():
+    temp = algo_grammar.AddrParser('3860 3880 and 3898 tamiami trl n')
+    assert temp.get_addrs() == ['3860 tamiami trl n', '3880 tamiami trl n', '3898 tamiami trl n']
+
+
+def test_through():
+    temp = algo_grammar.AddrParser('1031 through 1043 ster')
+    assert temp.get_addrs() == ['1031-1043 ster']
+'''
+
+
+
+
+']
+def test_basicParse():
+    temp = algo_grammar.AddrParser('8 Queensland Ct')
+    assert temp.get_addrs() == ['8 queensland ct']
+def test_basicParse():
+    temp = algo_grammar.AddrParser('8 Queensland Ct')
+    assert temp.get_addrs() == ['8 queensland ct']
+def test_basicParse():
+    temp = algo_grammar.AddrParser('8 Queensland Ct')
+    assert temp.get_addrs() == ['8 queensland ct']
+def test_basicParse():
+    temp = algo_grammar.AddrParser('8 Queensland Ct')
+    assert temp.get_addrs() == ['8 queensland ct']
+def test_basicParse():
+    temp = algo_grammar.AddrParser('8 Queensland Ct')
+    assert temp.get_addrs() == ['8 queensland ct']
+def test_basicParse():
+    temp = algo_grammar.AddrParser('8 Queensland Ct')
+    assert temp.get_addrs() == ['8 queensland ct']
+'''
---
+++ tests/geocoders_test.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 31 08:09:36 2020
+
+@author: fisherd
+"""
+
+import os
+
+os.chdir("C:\\Users\\fisherd\\Desktop\\projects\\CREDA\\")
+
+import pytest
+
+from CREDA_tools.geocoding import validators
+
+def test_canAssertTrue():
+    assert True
+
+def test_canCreateObject():
+    temp = validators.ValidatorFactory()
+    assert type(temp) is validators.ValidatorFactory
+    
+def test_ArcGisFormat():
+    pass
+
+def test_LightboxFormat():
+    pass
+    
+def test_CensusFormat():
+    pass
+    
+def test_censusLiveFormat():
+    pass
+
+def test_GapiFormat():
+    pass
+
+def test_GapiLiveFormat():
+    pass
+
+def test_otherGeocoderFormat():
+    pass
