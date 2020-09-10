6804585e93bd89027d81f55c1a8f87e5113a838a
fishinvest <fishinvest@gmail.com>
2020-09-10 11:55:13 -0400

    Adding address parser files

---
+++ tests/address_parser_tests.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 31 07:09:51 2020
+
+@author: fisherd
+"""
+
+import os
+
+os.chdir("C:\\Users\\fisherd\\Desktop\\projects\\CREDA\\")
+
+import pytest
+
+from CREDA_tools.address_parsing import algo_grammar
+
+def test_canAssertTrue():
+    assert True
+    
+    
+test_canAssertTrue()
