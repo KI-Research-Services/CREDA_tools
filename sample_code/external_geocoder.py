93ba5fd1a0d95e31d38073e0be1561eeebbab246
fishinvest <fishinvest@gmail.com>
2020-09-28 07:32:48 -0400

    Added 2 sample code files and two ArcGIS outputs for example run-throughs

---
+++ sample_code/external_geocoder.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 25 23:42:19 2020
+
+@author: fisherd
+
+This file is meant as an example of a basic CREDA_tools run using outside Geocoders.
+This employs ArcGIS, a solid industry standard.
+"""
+
+from CREDA_tools import helper
+
+
+# Begin run on dataset 1 through creation of file to Geocode
+# NOTE: Below are in windows relative paths. Modify as needed for Linux/Mac or absolute pass
+project = helper.CREDA_Project("test_data\\san_jose_d1.csv")
+project.clean_addresses()
+project.make_geocoder_file("for_geocoding.csv")
+
+#Begin run on dataset 2
+project2 = helper.CREDA_Project("test_data\\san_jose_d2.csv")
+project2.clean_addresses()
+project2.make_geocoder_file("for_geocoding2.csv")
+
+#####Run Geocoding on for_geocoding.csv and for_geocoding2.csv#####
+
+project.add_geocoder_results('ArcGIS', 'Arc_output.csv')
+project2.add_geocoder_results('ArcGIS', 'Arc_output2.csv')
+
+#Run rest of steps to UBIDs on dataset 1
+project.assign_shapefile("test_data\\san_jose_shapes.csv")
+project.perform_piercing()
+project.pick_best_match()
+project.generate_UBIDs()
+
+#Run rest of steps to UBIDs on dataset 2
+project2.assign_shapefile("test_data\\san_jose_shapes.csv")
+project2.perform_piercing()
+project2.pick_best_match()
+project2.generate_UBIDs()
+
+
+#Use a Jaccard join to combine into one dataset
+results = project.jaccard_combine(project2)
---
+++ sample_code/internal_geocoder.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 19 15:24:43 2020
+
+@author: fisherd
+This file is meant as an example of a basic CREDA_tools run using a Geocoder run from withing the program
+Since most geocoders require private access / API keys, we use the free Census geocoder here
+"""
+
+from CREDA_tools import helper
+
+# Begin run on dataset 1 by parsing addresses
+project = helper.CREDA_Project("test_data\\san_jose_d1.csv")
+project.clean_addresses()
+
+# Add a Census geocoding
+project.run_geocoding('Census')
+project.assign_shapefile("test_data\\san_jose_shapes.csv")
+project.perform_piercing()
+
+# Pick best match sets up the correct shapefile to generate a UBID from
+# We are only using a single geocoder, so this step will be fast.
+project.pick_best_match()
+project.generate_UBIDs()
+
+# Repeat all steps for the second dataset
+project2 = helper.CREDA_Project("test_data\\san_jose_d2.csv")
+project2.clean_addresses()
+
+project2.run_geocoding('Census')
+project2.assign_shapefile("test_data\\san_jose_shapes.csv")
+project2.perform_piercing()
+project2.pick_best_match()
+project2.generate_UBIDs()
+
+# Produce results, a jaccard joined combination of the two datasets based on UBID
+results = project.jaccard_combine(project2)
