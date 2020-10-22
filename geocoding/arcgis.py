dc5c0bfd808be3043306722f796e75196434ee97
fishinvest <fishinvest@gmail.com>
2020-10-22 01:13:17 -0400

    Linting improvements to validators

--- geocoding/arcgis.py
+++ geocoding/arcgis.py
@@ -13,7 +13,7 @@ from CREDA_tools.geocoding import validators
 
 class ArcGISValidator(validators.AddressValidator):
     '''This class runs data through the ArcGIS Validator/Geocoder'''
-    
+
     score_dict = {'SubAddress':0.95,
                   'PointAddress':0.90,
                   'StreetAddress':0.80,
@@ -21,20 +21,31 @@ class ArcGISValidator(validators.AddressValidator):
                   'StreetName':0.60,
                   'Locality':0.50
                   }
-    
+
     def __init__(self, geocode_file):
         super().__init__()
         self.geocode_file = geocode_file
         self.add_data()
-        
+
     def add_data(self):
+        '''
+        This is the standard geocoder function to add the file data as geocoder results
+
+        Returns
+        -------
+        None.
+
+        '''
         if os.path.exists(self.geocode_file):
             ArcGIS_results = pd.read_csv(self.geocode_file)
             ArcGIS_results = ArcGIS_results[['USER_TempIDZ', 'Addr_type', 'DisplayX', 'DisplayY']]
             ArcGIS_results['ArcGIS_confidence'] = 0
             for key, value in self.score_dict.items():
                 ArcGIS_results.loc[ArcGIS_results['Addr_type'] == key, 'ArcGIS_confidence'] = value
-            ArcGIS_results.rename(columns = {'DisplayY':'ArcGIS_lat', 'DisplayX':'ArcGIS_long', 'USER_TempIDZ': 'TempIDZ'}, inplace=True)
+            ArcGIS_results.rename(columns={'DisplayY':'ArcGIS_lat',
+                                           'DisplayX':'ArcGIS_long',
+                                           'USER_TempIDZ': 'TempIDZ'},
+                                  inplace=True)
             ArcGIS_results.set_index('TempIDZ', inplace=True)
             self.address_df = ArcGIS_results[['ArcGIS_long', 'ArcGIS_lat', 'ArcGIS_confidence']]
         else:
