c12981963e6218a758db36534f922c58f94992d5
fishinvest <fishinvest@gmail.com>
2020-12-05 11:15:00 -0500

    closes #13

--- sample_code/parcel_start.py
+++ sample_code/parcel_start.py
@@ -3,7 +3,11 @@
 Created on Sat Dec  5 00:29:44 2020
 
 @author: fisherd
+
+This is a VERY basic script showing a shape start. Really this just creates UBIDs
+and saves.
 """
+
 
 from CREDA_tools import helper
 
